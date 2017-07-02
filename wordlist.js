0fe98bd72c68a53b6bcb0da0e47305536e3ac5d7
Schezuk <schezuk@163.com>
2017-07-03 04:37:39 +0800

    Update wordlist.js

--- wordlist.js
+++ wordlist.js
@@ -1,2 +1,2 @@
-choices=["aaa","bbb","ccc","ddd","eee"];
-answers=["11111111111","22222222222","333333333","444","555"];
+choices=["caring","bare","niff","halt","lordy"];
+answers=["an exclamation showing surprise, "a part of your body that is bare is not covered by any clothes. When someone is not wearing any clothes at all, they are naked or nude","an unpleasant smell",shock etc","kind, helpful, and sympathetic towards other people","a temporary or permanent stop in a process"];
