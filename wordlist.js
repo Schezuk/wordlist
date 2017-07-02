0c5799a602a9808e4538c2c63e7dd247bf705e3a
Schezuk <schezuk@163.com>
2017-07-03 04:39:01 +0800

    Update wordlist.js

--- wordlist.js
+++ wordlist.js
@@ -1,2 +1,2 @@
 choices=["caring","bare","niff","halt","lordy"];
-answers=["an exclamation showing surprise, "a part of your body that is bare is not covered by any clothes. When someone is not wearing any clothes at all, they are naked or nude","an unpleasant smell",shock etc","kind, helpful, and sympathetic towards other people","a temporary or permanent stop in a process"];
+answers=["an exclamation showing surprise", "a part of your body that is bare is not covered by any clothes. When someone is not wearing any clothes at all, they are naked or nude","an unpleasant smell,shock etc","kind, helpful, and sympathetic towards other people","a temporary or permanent stop in a process"];
